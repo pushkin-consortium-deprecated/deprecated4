7cb2b5205292e0265a263a4c1b397ae9393ecae8
Xiaolu Bai <icebunnydoll@gmail.com>
2017-01-18 16:04:02 -0800

    cleans up console.log

--- pages/whichenglish/components/content/multipicture.js
+++ pages/whichenglish/components/content/multipicture.js
@@ -5,9 +5,38 @@ import { connect } from 'react-redux';
 import { userResponse } from '../../../../actions/userresponses';
 
 class MultiPicture extends React.Component {
-  constructor(props) {
-    super(props);
-    this.getChocieId = this.getChocieId.bind(this);
+  componentDidMount() {
+    const props = this.props;
+    const page_1_questions = [this.props.question];
+    const page_1_options = this.props.choices;
+    const multi_choice_block = {
+      type: 'survey-multi-picture',
+      questions: [page_1_questions],
+      options: [page_1_options],
+      required: [true, false],
+      on_finish: function(data) {
+        const response = JSON.parse(data.responses);
+        let choiceId;
+        props.choices.filter(currentChoice => {
+          if(currentChoice.url === response.answer){
+            choiceId = currentChoice.choiceId;
+          }
+        })
+        props.dispatch(userResponse({
+          choiceId: choiceId,
+          questionId: props.questionId,
+          userId: props.userId,
+        }));
+      },
+    };
+    jsPsych.init({
+      display_element: this.refs.main,
+      timeline: [multi_choice_block],
+      on_finish: function() {
+        props.nextQuestion();
+        props.progress();
+      },
+    });
   }
   componentDidUpdate() {
     const props = this.props;
@@ -26,7 +55,6 @@ class MultiPicture extends React.Component {
             choiceId = currentChoice.choiceId;
           }
         })
-        console.log("response.answer", response.answer)
         props.dispatch(userResponse({
           choiceId: choiceId,
           questionId: props.questionId,
@@ -42,48 +70,6 @@ class MultiPicture extends React.Component {
         props.progress();
       },
     });
-  }
-  componentDidMount() {
-    const props = this.props;
-    const page_1_questions = [this.props.question];
-    const page_1_options = this.props.choices;
-    const multi_choice_block = {
-      type: 'survey-multi-picture',
-      questions: [page_1_questions],
-      options: [page_1_options],
-      required: [true, false],
-      on_finish: function(data) {
-        const response = JSON.parse(data.responses);
-        let choiceId;
-        props.choices.filter(currentChoice => {
-          if(currentChoice.url === response.answer){
-            choiceId = currentChoice.choiceId;
-          }
-        })
-        console.log("response.answer", response.answer)
-        props.dispatch(userResponse({
-          choiceId: choiceId,
-          questionId: props.questionId,
-          userId: props.userId,
-        }));
-      },
-    };
-    jsPsych.init({
-      display_element: this.refs.main,
-      timeline: [multi_choice_block],
-      on_finish: function() {
-        props.nextQuestion();
-        props.progress();
-      },
-    });
-  }
-  getChocieId(response) {
-    const props = this.props;
-    return props.choices.filter(currentChoice => {
-      if(currentChoice.url === response.url){
-        return currentChoice.id
-      }
-    })
   }
   render() {
     console.log("this.props multipicture", this.props)
