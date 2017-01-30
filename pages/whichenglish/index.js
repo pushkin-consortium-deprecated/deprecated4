1d0a6014991c07e250e07f79296500ee7c43c9f2
Rob Wilkinson <wilkinson.robert.a@gmail.com>
2017-01-30 15:13:43 -0800

    cleans up and removes unneeded window listeners

--- pages/whichenglish/index.js
+++ pages/whichenglish/index.js
@@ -8,47 +8,16 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import * as f from 'react-foundation';
-import * as b from 'react-bootstrap';
+import { Image } from 'react-bootstrap';
 import React, { PropTypes } from 'react';
-import s from './styles.css';
 import StartPage from './components/startpage';
 
 class HomePage extends React.Component {
-
-  constructor() {
-    super();
-    this.state = {};
-    this.updateDimensions = this.updateDimensions.bind(this);
-  }
-
-  componentWillMount() {
-    window.addEventListener('resize', this.updateDimensions);
-    this.updateDimensions();
-  }
-
-  componentDidMount() {
-    document.title = 'Games With Words';
-    this.updateDimensions();
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateDimensions);
-  }
-
-  updateDimensions() {
-    if (window.innerWidth < 992) {
-      this.setState({ mobile: true, border: { borderBottom: 'dashed #a9a9a9' } });
-    } else {
-      this.setState({ mobile: false, border: { borderRight: 'dashed #a9a9a9' } });
-    }
-  }
-
   render() {
     return (
       <div>
         {/* needed for sticky footer */}
-        <b.Image style={{ display: 'none' }} src="/../../img/favicon.ico" />
+        <Image style={{ display: 'none' }} src="/../../img/favicon.ico" />
         <div>
           <StartPage />
         </div>
